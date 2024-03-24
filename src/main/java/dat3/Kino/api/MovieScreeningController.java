f9a01ad3b59bf869c5599a393c807b4f1a189761
MaryanQ <maryansalad7@outlook.dk>
2024-03-24 23:25:48 +0100

    initial commit

--- src/main/java/dat3/Kino/api/MovieScreeningController.java
+++ src/main/java/dat3/Kino/api/MovieScreeningController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/movie-screenings")
+@RequestMapping("/movie_screenings")
 public class MovieScreeningController {
 
     @Autowired
